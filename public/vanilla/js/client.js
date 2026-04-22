f01152a2d4e69b6060c00ded3fc33f4e4bce56f7
dustinpfister <dustin.pfister@gmail.com>
2026-04-22 09:31:24 -0400

    using color tag css

--- public/vanilla/js/client.js
+++ public/vanilla/js/client.js
@@ -173,13 +173,47 @@ get_config()
     el_cs.appendChild(opt)
   });
   
-  const cs = CONFIG.color_status;
-  const i_array = cs.i_array;
-  const colorObj = CONFIG.COLOR_CONF.array[ i_array ];
-  const printColorObj = colorObj.data[cs.i_print];
-  const print_color = CONFIG.print_color = printColorObj.desc.toLowerCase(); 
+  //const cs = CONFIG.color_status;
+  //const i_array = cs.i_array;
+  //const colorObj = CONFIG.COLOR_CONF.array[ i_array ];
+  //const printColorObj = colorObj.data[cs.i_print];
+  //const print_color = CONFIG.print_color = printColorObj.desc.toLowerCase(); 
   
-  el_out.innerHTML = get_html_color_status_string( CONFIG.COLOR_CONF, cs, new Date() );
+  //el_out.innerHTML = get_html_color_status_string( CONFIG.COLOR_CONF, cs, new Date() );
+  
+  //el_out.innerHTML = '';
+  
+  const conf = CONFIG.COLOR_CONF;
+const stat = CONFIG.color_status;
+const color_setting = conf.array[ stat.i_array ];
+const color_print = color_setting.data[ stat.i_print ];
+const color_cull = color_setting.data[ stat.i_cull ];
+const color_disc = stat.disc.map( (arr)=> {  return { off: arr[0], color:  color_setting.data[ arr[1] ] } });
+
+[
+  { color: color_print, off: 'PRINT', title: 'PRINT NEW ITEMS', mess: 'NEW<br> ITEMS<br> ARE<br> THIS<br> TAG' },
+  { color: color_cull, off: 'CULL', title: 'CULL ITEMS', mess: 'CULL<br> OR<br> REPRICE<br> THIS<br> TAG' },
+    { color: color_disc[0].color, off: color_disc[0].off + '%', title: 'COLOR TAG DISCOUNT' },
+  { color: color_disc[1].color, off: color_disc[1].off + '%', title: 'COLOR TAG DISCOUNT' }
+].forEach((a)=>{
+  el_tag = document.createElement('div');
+  el_tag.className = 'tag';
+  el_tag.style.background = a.color.web;
+  [ 
+    ['tag_title', a.title],
+    ['tag_percent', a.off],
+    ['tag_mess_text', a.mess || 'ALL<br> ITEMS<br> WITH<br> THIS<br> TAG'],
+    ['tag_color_text', a.color.desc]
+  ].forEach((b)=>{
+    const el_child = document.createElement('div');
+    el_child.className = b[0];
+    el_child.innerHTML = b[1];
+    el_tag.appendChild(el_child);
+  });
+  el_out.appendChild(el_tag);
+});
+
+  
   
   el_submit_item.addEventListener('click', ( ) => {
     post_item(el_ds.value, el_ps.value, el_cs.value)
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ import express from 'express';
 const router_index = express.Router();
 
 router_index.get('/', (req, res) => {
-  res.render('index', { username: req.user.username });
+  res.render('index', { 
+    username: req.user.username
+  });
 });
 
 export { router_index };
